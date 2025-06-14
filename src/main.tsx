61aa7860496bad439621e8c7a7699f440d72761e
k1shan <93940049+k1shan-k@users.noreply.github.com>
2025-06-14 20:27:50 +0530

    Add Bootstrap JavaScript for Dropdown Functionality

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Import Bootstrap JavaScript for dropdown functionality
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
